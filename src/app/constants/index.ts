159b417cd76d5b110ae315ee8471dde2e20bf854
unknown <denilsondiniz341@gmail.com>
2023-12-14 21:37:49 +0200

    Primeiro commit

---
+++ src/app/constants/index.ts
@@ -0,0 +1,48 @@
+export const inclusions = [
+    {
+      title: 'Frete Grátis',
+      description: 'Frete Grátis para compras acima de $150',
+      icon: '/assets/icons/shipping.svg',
+    },
+    {
+      title: 'Garantia Monetária',
+      description: 'Dentro de 30 dias para a troca',
+      icon: '/assets/icons/dollar.svg',
+    },
+    {
+      title: 'Online Support',
+      description: '24 por dia, 7 dias por semana',
+      icon: '/assets/icons/support.svg',
+    },
+    {
+      title: 'Pagamento Flexivel',
+      description: 'Pague com múltilplos catões de crédido',
+      icon: '/assets/icons/payment.svg',
+    },
+  ]
+  
+  export const profileNavItems = [
+    {
+      title: 'Informação Pessoal',
+      url: '/account',
+      icon: '/assets/icons/user.svg',
+    },
+    {
+      title: 'Minhas Compras',
+      url: '/account/purchases',
+      icon: '/assets/icons/purchases.svg',
+    },
+    {
+      title: 'Meus Pedidos',
+      url: '/account/orders',
+      icon: '/assets/icons/orders.svg',
+    },
+    {
+      title: 'Logout',
+      url: '/logout',
+      icon: '/assets/icons/logout.svg',
+    },
+  ]
+  
+  export const noHeaderFooterUrls = ['/create-account', '/login', '/recover-password']
+  
